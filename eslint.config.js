2e27290b17ddafd154adca5dbff4e7a9d93724c3
takumines <takumines1@gmail.com>
2025-11-03 17:35:43 +0900

    setup storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import prettier from 'eslint-config-prettier'
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import unusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
+  storybook.configs['flat/recommended'],
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -17,26 +20,26 @@ export default defineConfig([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       tseslint.configs.recommendedTypeChecked,
-      prettier
+      prettier,
     ],
     plugins: {
       'unused-imports': unusedImports,
     },
     rules: {
       // eslint-plugin-unused-importsで未使用変数を検出するため、@typescript-eslint/no-unused-varsを無効化
-      "@typescript-eslint/no-unused-vars": "off",
+      '@typescript-eslint/no-unused-vars': 'off',
       // eslint-plugin-unused-importsで未使用インポートを検出するため、unused-imports/no-unused-importsを有効化
-      "unused-imports/no-unused-imports": "error",
+      'unused-imports/no-unused-imports': 'error',
       // eslint-plugin-unused-importsで未使用変数を検出する設定
-      "unused-imports/no-unused-vars": [
-            "warn",
-            {
-                "args": "after-used",
-                "argsIgnorePattern": "^_",
-                "vars": "all",
-                "varsIgnorePattern": "^_"
-            },
-        ]
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -47,4 +50,4 @@ export default defineConfig([
       },
     },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest/config" />
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
+// https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+const dirname =
+  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [
     react({
@@ -12,4 +21,32 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
+        },
+      },
+    ],
+  },
 });
